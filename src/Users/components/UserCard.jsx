80d1761be32251ff6973caf00c43f1189245bc37
Juansitoh <juanfergamer77@gmail.com>
2023-08-24 15:27:50 -0500

    user card

--- src/Users/components/UserCard.jsx
+++ src/Users/components/UserCard.jsx
@@ -1,13 +1,24 @@
-import react from "react";
-import { useContext } from "react";
-import { useUsers } from "../Context/userContext";
 
 export function UserCard({user}) {
   return (
     <div className=" bg-orange-600 h-20 w-34">
-        <h1 className=" text-green-700">{user.Nombre}</h1>
-        <h1 className=" text-green-700">{user.Apellidos}</h1>
-        <h1 className=" text-green-700">{user.Email}</h1>
+        <div>
+            <p> </p>
+        </div>
+
+        <div>
+            <p>{user.userName}</p>
+            <p>{user.userLastName}</p>
+        </div>
+
+        <div>
+            <p> Correo electrónico </p>
+            <p>{user.userEmail}</p>
+        </div>
+
+        <div>
+            
+        </div>
     </div>
   )
 }
