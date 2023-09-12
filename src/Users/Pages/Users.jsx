05441496b3aba60c4f9ead37e93a4bb9c41ccf8d
Juanfemon23 <ElFuanfex@hotmail.com>
2023-09-12 15:28:57 -0500

    Search component included in users

--- src/Users/Pages/Users.jsx
+++ src/Users/Pages/Users.jsx
@@ -8,6 +8,7 @@ import { RolesRegister } from "../../Roles/components/rolesRegister";
 import { useRoles } from "../../Roles/context/rolesContext";
 import { CardRoles } from "../../Roles/components/CardRoles";
 import { Navigate } from "react-router-dom";
+import { Search } from "../../components/globalComponents/Search";
 
 export function Users() {
       const {getUsers , createUser} = useUsers();
@@ -35,7 +36,7 @@ export function Users() {
           </div>
           <div className=' w-[99rem] h-1.5 border-3 border-[#252525]'> </div>
           <p className=" m-8 font-bold text-[20px]"> Usuarios creados</p>
-          
+          <Search  label={'Buscar usuario'} placeholder={''} />
           <div className="flex flex-wrap">
             <CardUsers/>
           </div>
