9feef34071791fb293ab52302c21628dc096e401
JuanfeMon23 <ELFuanfex@hotmail.com>
2023-11-28 22:48:12 -0500

    updating environment variables

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const conection = axios.create({
-    baseURL : 'http://localhost:3000/api',
+    baseURL : import.meta.env.VITE_BACKEND || 'http://localhost:3000/api',
     withCredentials : true
 });
 
