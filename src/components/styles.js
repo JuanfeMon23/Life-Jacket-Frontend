5edb2c64053c481289412e53c26937abb10e2e4f
Yenifer Salazar <yensalazarrestrepo@gmail.com>
2023-12-04 20:20:39 -0500

    update responsive in manuals

--- src/components/styles.js
+++ src/components/styles.js
@@ -4,5 +4,8 @@ export const styles = {
     firstP : 'text-xs md:text-sm font-bold',
     thirdContainer : 'items-end border-2 border-indigo-500/75 rounded-lg w-[7rem] md:w-[15rem] p-1',
     secondP : 'text-center',
-    largeP : 'border-2 border-indigo-500/75 rounded-lg p-1 w-[15rem] md:w-[20rem]'
+    largeP : 'border-2 border-indigo-500/75 rounded-lg p-1 w-[15rem] md:w-[20rem]',
+    subtitles: 'text-black font-bold sm:mt-10 mb-5 ml-10',
+    parr: 'text-black m-4 ml-10 mr-10',
+    img: 'mx-auto w-full  sm:w-100 lg:w-80 xl:w-100 md:w-80 md:h-70 rounded-8 border border-solid border-F4F2F7',
 }
